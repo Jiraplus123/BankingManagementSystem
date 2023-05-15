f0dac1b92ca9b5f71f85034539df01d04431debf
Jiraplus0304 <jiraplus.chan@mail.kmutt.ac.th>
2023-05-15 22:58:38 +0700

    updated file

---
+++ BankingConnection.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bankingapplication1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class BankingConnection {
+    public static Connection connect(){
+        String URL = "jdbc:mysql://localhost:3306/mydb1";
+        String username = "root";
+        String password = "26390447";
+        Connection connection = null;
+        
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(URL, username, password);
+            
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(BankingConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return connection;
+    }
+}
